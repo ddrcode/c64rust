199d83db2c95b9fe845bb0a9707560c9c5cbd145
ddrcode <ddrcode@gmail.com>
2023-09-09 16:29:27 +0100

    adjusted ASM tests to use variables rather than hardcoded addresses

--- tests/run.sh
+++ tests/run.sh
@@ -2,11 +2,21 @@
 
 ASM="./bin/acme"
 EMU="../target/debug/c64emu"
-ADDR="0600"
+ADDR="0000"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+LIGHT_BLUE='\033[1;34m'
+RC='\033[0m' # Reset Color
+
+OK_MSG="   ${GREEN}OK${RC}"
+FAILED_MSG="   ${RED}Failed!${RC}"
+
+mkdir -p ./asm-out/
 
 run_test() {
     local file="$1"
-    $ASM --cpu 6502 --setpc 600 -o "./asm-out/${file}.o" "./asm/${file}.a"
+    $ASM --cpu 6502 --setpc $ADDR -o "./asm-out/${file}.o" "./asm/${file}.a"
     local res=$($EMU --ram "./asm-out/${file}.o" --ram-file-addr $ADDR --start-addr $ADDR --show-status | sed 's/\$//g')
     printf -- "%s" "$res"
 }
@@ -24,7 +34,7 @@ test() {
     local expected_p="$3"
 
     echo ""
-    echo "Running test for ${file}.a"
+    echo -e "Running test for ${LIGHT_BLUE}${file}.a${RC}"
     local res=$(run_test "$file")
     local reg_a=$(echo "$res" | sed 's/.*A:\(..\).*/\1/')
     local reg_p=$(echo "$res" | sed 's/.*P:\(.*\)/\1/')
@@ -35,9 +45,9 @@ test() {
     echo "   Expected P: ${expected_p}, Expected A: ${expected_a}"
     echo "   Actual P:   ${reg_p}, Actual A:   ${reg_a}"
 
-    assert "$expected_a" "$reg_a"  && echo "   failed" && exit 1
-    assert "$expected_p" "$reg_p"  && echo "   failed" && exit 1
-    echo "   OK!"
+    assert "$expected_a" "$reg_a"  && echo -e "$FAILED_MSG" && exit 1
+    assert "$expected_p" "$reg_p"  && echo -e "$FAILED_MSG" && exit 1
+    echo -e "$OK_MSG"
     return 0
 }
 
