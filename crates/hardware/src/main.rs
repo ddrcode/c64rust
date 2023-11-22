f64c5adb6aec599ceba147169e278b4ea0bcc264
ddrcode <ddrcode@gmail.com>
2023-11-22 23:34:30 +0000

    working version

--- crates/hardware/src/main.rs
+++ crates/hardware/src/main.rs
@@ -4,6 +4,8 @@ use std::io::Read;
 use std::{fs::File, path::PathBuf};
 use anyhow::Result;
 
+const STEP: Duration = Duration::from_micros(10000);
+
 const ADDR_PINS: [u8; 16] = [4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 21, 20, 16, 12];
 const DATA_PINS: [u8; 8] = [14, 15, 18, 23, 24, 25, 8, 7];
 const CLOCK_PIN: u8 = 3;
@@ -19,35 +21,45 @@ fn main() -> Result<()> {
 
     let mut clock_pin = Gpio::new()?.get(CLOCK_PIN)?.into_output();
 
-    let program = get_file_as_byte_vec(&PathBuf::from(r"./tests/target/add-sub-16bit.p"))?;
+//    let program = get_file_as_byte_vec(&PathBuf::from(r"./tests/target/add-sub-14bit.p"))?;
+    let rom = get_file_as_byte_vec(&PathBuf::from(r"./rom/kernal-64c.251913-01.bin"))?;
+    let kernal = &rom[8192..];
+    let basic = &rom[..8192];
+    let blank = [0u8; 0x2000];
+    let program = [basic, &blank, kernal].concat();
+
 
     let mut ram = [0u8; 65536];
-    ram[0xfffc] = 0x00;
-    ram[0xfffd] = 0x02;
+    ram[0xfffc] = 0xe2;
+    ram[0xfffd] = 0xfc;
     let mut i = 0;
     for cell in program {
-        ram[0x200 + i] = cell;
+        ram[0xa000 + i] = cell;
         i += 1;
     }
 
-    for cycle in 0..1000 {
+    for cycle in 0..4000000 {
         clock_pin.toggle();
-        std::thread::sleep(Duration::from_millis(300));
+        std::thread::sleep(STEP);
         if rw_pin.is_high() {
             set_mode(&mut data_pins, Mode::Output);
         } else {
             set_mode(&mut data_pins, Mode::Input);
         }
 
+        ram[0xd012] = (cycle % 255) as u8;
+
         let addr = read_word(&addr_pins);
         if data_pins[0].mode() == Mode::Output {
             write_byte(&mut data_pins, ram[addr as usize]);
-            println!("Writing from [{:05}] {:04x}: {:02x}", cycle, addr, read_byte(&data_pins));
+            println!("[{:07}] Writing from {:04x}: {:02x}", cycle, addr, read_byte(&data_pins));
         } else {
             ram[addr as usize] = read_byte(&data_pins);
-            println!("Reading from [{:05}] {:04x}: {:02x}", cycle, addr, read_byte(&data_pins));
+            println!("[{:07}] Reading from {:04x}: {:02x}", cycle, addr, read_byte(&data_pins));
         }
     }
+
+    print_screen(&ram);
 
     Ok(())
 }
@@ -84,10 +96,38 @@ fn set_mode(pins: &mut [IoPin], mode: Mode) {
     });
 }
 
-pub fn get_file_as_byte_vec(filename: &PathBuf) -> Result<Vec<u8>> {
+fn get_file_as_byte_vec(filename: &PathBuf) -> Result<Vec<u8>> {
     let mut f = File::open(filename)?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
     Ok(buffer)
 }
 
+fn print_screen(mem: &[u8]) {
+    let char_set: Vec<char> =
+        "@abcdefghijklmnopqrstuvwxyz[£]↑← !\"#$%&'()*+,-./0123456789:;<=>?\
+         -ABCDEFGHIJKLMNOPQRSTUVWXYZ[£]↑← !\"#$%&'()*+,-./0123456789:;<=>?\
+         @ABCDEFGHIJKLMNOPQRSTUVWXYZ[£]↑← !\"#$%&'()*+,-./0123456789:;<=>?\
+         -abcdefghijklmnopqrstuvwxyz[£]↑← !\"#$%&'()*+,-./0123456789:;<=>?"
+            .chars()
+            .collect();
+    let mut n = 0;
+    println!();
+    println!("{}", " ".repeat(44));
+    print!("{}", "  ");
+    for i in 0x0400..0x07e8 {
+        let sc = mem[i];
+        print!(
+            "{}",
+            format!("{}", char_set[sc as usize])
+        );
+        n += 1;
+        if n % 40 == 0 {
+            print!("{}", "  ");
+            println!();
+            print!("{}", "  ");
+        }
+    }
+    println!("{}", " ".repeat(42));
+    println!("              ");
+}
